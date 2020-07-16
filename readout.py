3940584af26b43e2fd23370e1aa40ced3ecd4c1f
davidsudm <davidsudm@gmail.com>
2020-07-16 19:44:28 +0200

    readout update : - histogram of times fixed - 2D histogram for waveforms fixed and color bar added - threshold condition added, if all waveforms less than minimum threshold value then discard event

--- readout.py
+++ readout.py
@@ -43,6 +43,7 @@ def give_list_of_file(input_dir):
 
 def read_data(file, debug=False):
 
+    recycled_events = 0
     with DRS4BinaryFile(file) as f:
         board_id = f.board_ids[0]
         active_channels = f.channels
@@ -56,8 +57,6 @@ def read_data(file, debug=False):
                           np.cumsum(time_widths[1]),
                           np.cumsum(time_widths[2]),
                           np.cumsum(time_widths[3])])
-
-        # times = np.insert(times, 0, 0, axis=1)
 
         for event in f:
 
@@ -75,7 +74,7 @@ def read_data(file, debug=False):
                                  event.adc_data[board_id][3],
                                  event.adc_data[board_id][4]])
 
-            # adc_data = np.insert(adc_data, 0, 0, axis=1)
+            adc_data = get_float_waveform(adc_data, event_rc)
 
             if debug is True:
                 print('board id : ', board_id)
@@ -100,9 +99,24 @@ def get_float_waveform(adc_waveform_matrix, range_center):
     # Get the range according to the range center defined
     amplitude_low_edge = -0.5 + range_center/1000.
     # Amplitude in volts
-    amplitude = adc_waveform_matrix/(np.power(2, 16)-1) + amplitude_low_edge
+    amplitude_matrix = adc_waveform_matrix/np.power(2, 16) + amplitude_low_edge
 
-    return amplitude
+    return amplitude_matrix
+
+
+def discard_event(amplitude_matrix, min_threshold=-0.10):
+    """
+    Tells if an event should based on the signal channels. If the signals channel doesn't pass a minimum threshold value,
+    the event is discarded.
+    :param amplitude_matrix:    the matrix containing the values of the waveform in mV for the 4 channels
+    :param min_threshold:       minimal threshold value. if all the waveform in the signal is lower than this value, the
+                                event is discarded
+    :return: boolean value
+    """
+    condition_ch1 = all(x < min_threshold for x in amplitude_matrix[1])
+    condition_ch3 = all(x < min_threshold for x in amplitude_matrix[3])
+    discard = condition_ch1 or condition_ch3
+    return discard
 
 
 def draw_waveforms(times, adc_data, labels):
@@ -142,14 +156,15 @@ def entry():
 
     waveforms = read_data(file=file, debug=False)
     # Waveform was set from -0.05 to 0.95 mV
-    bins_y = np.linspace(-0.10, 1.0, 221)
+    bins_y = np.linspace(-0.10, 1.0, 551)
     bin_edges = []
 
     event_counter = 0
+    discarded_events = 0
 
     for time_widths, times, adc_data, event_rc in waveforms:
 
-        if event_counter is 0:
+        if event_counter == 0:
             times = np.insert(times, 0, 0, axis=1)
             times = np.delete(times, -1, axis=1)
             bin_edges = time_widths / 2 + times
@@ -157,10 +172,8 @@ def entry():
         if event_counter % 1000 == 0:
             print('Event id :', event_counter)
 
-        adc_data = get_float_waveform(adc_data, event_rc)
-
-        threshold = 15./1000.
-        if (all(x < threshold for x in adc_data[1]) or all(x < threshold for x in adc_data[3])) is True:
+        if discard_event(amplitude_matrix=adc_data, min_threshold=0.015) is True:
+            discarded_events += 1
             continue
         else:
             temp_histo2d_ch0, xedges_ch0, yedges_ch0 = np.histogram2d(times[0], adc_data[0], bins=[bin_edges[0], bins_y])
@@ -169,17 +182,25 @@ def entry():
             temp_histo2d_ch3, xedges_ch3, yedges_ch3 = np.histogram2d(times[3], adc_data[3], bins=[bin_edges[3], bins_y])
 
         if event_counter == 0:
-            histo2d_ch0 = np.zeros_like(temp_histo2d_ch0)
-            histo2d_ch1 = np.zeros_like(temp_histo2d_ch1)
-            histo2d_ch2 = np.zeros_like(temp_histo2d_ch2)
-            histo2d_ch3 = np.zeros_like(temp_histo2d_ch3)
+            histo2d_ch0 = np.zeros_like(temp_histo2d_ch0, dtype=int)
+            histo2d_ch1 = np.zeros_like(temp_histo2d_ch0, dtype=int)
+            histo2d_ch2 = np.zeros_like(temp_histo2d_ch0, dtype=int)
+            histo2d_ch3 = np.zeros_like(temp_histo2d_ch0, dtype=int)
+
+            histo2d_ch0 += temp_histo2d_ch0.astype(int)
+            histo2d_ch1 += temp_histo2d_ch1.astype(int)
+            histo2d_ch2 += temp_histo2d_ch2.astype(int)
+            histo2d_ch3 += temp_histo2d_ch3.astype(int)
+
         else:
-            histo2d_ch0 += temp_histo2d_ch0
-            histo2d_ch1 += temp_histo2d_ch1
-            histo2d_ch2 += temp_histo2d_ch2
-            histo2d_ch3 += temp_histo2d_ch3
+            histo2d_ch0 += temp_histo2d_ch0.astype(int)
+            histo2d_ch1 += temp_histo2d_ch1.astype(int)
+            histo2d_ch2 += temp_histo2d_ch2.astype(int)
+            histo2d_ch3 += temp_histo2d_ch3.astype(int)
 
         event_counter += 1
+        if event_counter == 100000:
+            break
 
     histo2d = [histo2d_ch0, histo2d_ch1, histo2d_ch2, histo2d_ch3]
     xedges = [xedges_ch0, xedges_ch1, xedges_ch2, xedges_ch3]
@@ -190,24 +211,25 @@ def entry():
         anchored_text = AnchoredText(text, loc=1)
 
         fig, ax = plt.subplots()
-        ax.imshow(histo.T, origin='low', norm=LogNorm(),
-                  extent=[xedges[k][0], xedges[k][-1], yedges[k][0], yedges[k][-1]])
+        im = ax.imshow(histo.T, origin='low', cmap=plt.cm.viridis, norm=LogNorm(), extent=[xedges[k][0], xedges[k][-1], yedges[k][0], yedges[k][-1]])
         ax.set_aspect(200)
         ax.add_artist(anchored_text)
         ax.set_xlabel('Time [ns]')
         ax.set_ylabel('Amplitude [V]'.format(k))
+        cax = fig.add_axes([ax.get_position().x1 + 0.01, ax.get_position().y0, 0.02, ax.get_position().height])
+        plt.colorbar(im, cax=cax)
         plt.savefig('{}/waveforms_histo2d_ch{}.png'.format(output_dir, k))
         plt.close(fig)
 
     end_time = time.time()
     print("time of waveform process : ", end_time - start_time)
+    print("events : {}".format(event_counter - discarded_events))
 
     if debug:
-
         pdf_waveforms_plots = PdfPages('{}/waveforms_by_4.pdf'.format(output_dir))
-
         waveforms = read_data(file, debug=False)
         event_counter = 0
+
         for time_widths, times, adc_data, event_rc in waveforms:
             if event_counter is 0:
                 times = np.insert(times, 0, 0, axis=1)
@@ -254,30 +276,32 @@ def entry():
                 plt.savefig('{}/times_dist.png'.format(output_dir))
                 plt.close(fig)
 
-            adc_data = get_float_waveform(adc_data, event_rc)
-            if event_counter % 1000 == 0:
-                fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
-                # Triggers
-                axs[0, 0].plot(np.cumsum(time_widths[1]), adc_data[1], linestyle="solid", color='tab:red', label='signal : event {}'.format(event_counter))
-                axs[0, 0].legend()
-                axs[0, 0].set_ylim(-0.10, 1.00)
-                axs[0, 1].plot(np.cumsum(time_widths[3]), adc_data[3], linestyle="solid", color='tab:red', label='signal : event {}'.format(event_counter))
-                axs[0, 1].legend()
-                axs[0, 1].set_ylim(-0.10, 1.00)
-                # Signals
-                axs[1, 0].plot(np.cumsum(time_widths[0]), adc_data[0], linestyle="solid", color='tab:green', label='trigger')
-                axs[1, 0].legend()
-                axs[1, 0].set_ylim(-0.10,1.00)
-                axs[1, 1].plot(np.cumsum(time_widths[2]), adc_data[2], linestyle="solid", color='tab:green', label='trigger')
-                axs[1, 1].legend()
-                axs[1, 1].set_ylim(-0.10,1.00)
+            if event_counter < 100:
+                if discard_event(amplitude_matrix=adc_data, min_threshold=0.015) is True:
+                    continue
+                else:
+                    fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+                    # Triggers
+                    axs[0, 0].plot(np.cumsum(time_widths[1]), adc_data[1], linestyle="solid", color='tab:red', label='signal : event {}'.format(event_counter))
+                    axs[0, 0].legend()
+                    axs[0, 0].set_ylim(-0.10, 1.00)
+                    axs[0, 1].plot(np.cumsum(time_widths[3]), adc_data[3], linestyle="solid", color='tab:red', label='signal : event {}'.format(event_counter))
+                    axs[0, 1].legend()
+                    axs[0, 1].set_ylim(-0.10, 1.00)
+                    # Signals
+                    axs[1, 0].plot(np.cumsum(time_widths[0]), adc_data[0], linestyle="solid", color='tab:green', label='trigger')
+                    axs[1, 0].legend()
+                    axs[1, 0].set_ylim(-0.10, 1.00)
+                    axs[1, 1].plot(np.cumsum(time_widths[2]), adc_data[2], linestyle="solid", color='tab:green', label='trigger')
+                    axs[1, 1].legend()
+                    axs[1, 1].set_ylim(-0.10, 1.00)
 
-                axs[1, 0].set_xlabel(r'Time [ns]')
-                axs[1, 1].set_xlabel(r'Time [ns]')
-                axs[0, 0].set_ylabel('Amplitude [V]')
-                axs[1, 0].set_ylabel('Amplitude [V]')
-                pdf_waveforms_plots.savefig(fig)
-                plt.close(fig)
+                    axs[1, 0].set_xlabel(r'Time [ns]')
+                    axs[1, 1].set_xlabel(r'Time [ns]')
+                    axs[0, 0].set_ylabel('Amplitude [V]')
+                    axs[1, 0].set_ylabel('Amplitude [V]')
+                    pdf_waveforms_plots.savefig(fig)
+                    plt.close(fig)
 
             event_counter += 1
         pdf_waveforms_plots.close()
